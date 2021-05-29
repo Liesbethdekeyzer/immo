c8f75dc5acd52f85e9c706c0d3e7ee7afe0515ad
Liesbethdekeyzer <liesbethdekeyzer@gmail.com>
2021-05-29 11:23:14 +0200

    add config for app engine

--- src/main/java/com/liesbeth/immo/ImmoApplication.java
+++ src/main/java/com/liesbeth/immo/ImmoApplication.java
@@ -2,6 +2,8 @@ package com.liesbeth.immo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ImmoApplication {
@@ -10,4 +12,12 @@ public class ImmoApplication {
 		SpringApplication.run(ImmoApplication.class, args);
 	}
 
+	@RestController
+	class HelloWorldController {
+
+		@GetMapping("/")
+		public String hello() {
+			return "hello world!";
+		}
+	}
 }
